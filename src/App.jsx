bef2efc618c8611ba2a13918d6e4a37374d3e6ae
GuilhermeAlmeida <galmeidas2008@gmail.com>
2025-08-29 13:24:14 -0300

    atualizando a página de chamados dos técnicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 // src/App.jsx
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./router/routes";
-import { AuthProvider } from "./context/auth-context";
-
+import { AuthProvider } from "./context/auth-context";  
 export default function App() {
   return (
     <AuthProvider>
