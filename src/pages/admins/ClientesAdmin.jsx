99db69e18eeab8c330fac6b2b81be9492d21d0f8
Mariana Amorim <amorimmariana007@gmail.com>
2025-10-31 17:07:01 -0300

    Corrige layout clientes e tecnicos

--- src/pages/admins/ClientesAdmin.jsx
+++ src/pages/admins/ClientesAdmin.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
+import Botao from "../../components/Button";
+import { Plus } from "lucide-react";
+import { ModalCriarCliente } from "../../components/Modals/CriarCliente";
 import ClientTable from "../../components/ClientAdminTabe";
+import './styles.scss';
 
 export default function ClientesAdmin() {
-    return (
-        <>
-                <h1 className="header-clientes-admin">Clientes</h1>
-                <ClientTable />
-                <Outlet />
+  const [open, setOpen] = useState(false);
 
-        </>
-    )
+  return (
+    <>
+      <div className="header-container">
+        <h1 className="header-clientes-admin">Clientes</h1>
+        <Botao
+          className="botao-novo-cliente"
+          icon={Plus}
+          text="Novo Cliente"
+          onClick={() => setOpen(true)}
+        />
+      </div>
+
+      <ClientTable />
+      <Outlet />
+
+      <ModalCriarCliente isOpen={open} onClose={() => setOpen(false)} />
+    </>
+  );
 }
--- src/pages/admins/TecnicosAdmin.jsx
+++ src/pages/admins/TecnicosAdmin.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
 import TecnicosTable from "../../components/TableAdminTec";
+import Botao from "../../components/Button";
+import { Plus } from "lucide-react";
+import { ModalCriarTecnico } from "../../components/Modals/CriarTecnico";
 import './styles.scss';
+
 export default function TecnicosAdmin() {
-    return (
-        <>
+  const [open, setOpen] = useState(false);
 
-                <h1 className="header-tecnicos-admin">Técnicos</h1>
-                <TecnicosTable />
-                <Outlet />
+  return (
+    <>
+      <div className="header-container">
+        <h1 className="header-tecnicos-admin">Técnicos</h1>
+        <Botao
+          className="botao-novo-tecnico"
+          icon={Plus}
+          text="Novo Técnico"
+          onClick={() => setOpen(true)}
+        />
+      </div>
 
-        </>
-    )
+      <TecnicosTable />
+      <Outlet />
+
+      <ModalCriarTecnico isOpen={open} onClose={() => setOpen(false)} />
+    </>
+  );
 }
