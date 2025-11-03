7eff2cc2607f4d75c78959cc9253d7db78f6542e
Mariana Amorim <amorimmariana007@gmail.com>
2025-11-03 15:53:58 -0300

    Muda a url da api

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { jwtDecode } from "jwt-decode";
 
 // Cria instância do axios com baseURL da API
 const api = axios.create({
-  baseURL: "https://log-teste.onrender.com/",
+  baseURL: "https://api-sdc.onrender.com/",
 });
 
 // Adiciona token em toda requisição, se existir
